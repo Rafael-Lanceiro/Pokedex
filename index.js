af1be5aa354e720eb63a767581cb0593b1cfdad7
Rafael Lanceiro <lanceirorafael@gmail.com>
2022-07-09 13:19:17 -0300

    Requisitada API pronta, e inseridas funções HTML no index.js.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const getPokemonUrl = 'https://pokeapi.co/api/v2/pokemon/10';
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
 const fetchPokemon = () => {
-    fetch(getPokemonUrl) 
-        .then((response) => response.json())
-        .then((pokemon) => console.log(pokemon));
+    const pokemonPromises = [];
+
+    for (let i = 1; i <= 151; i++) {
+        pokemonPromises.push(
+            fetch(getPokemonUrl(i)).then((response) => response.json())
+        );
+    }
+
+    Promise.all(pokemonPromises).then((pokemons) => {
+        const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+            const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
+
+            accumulator += `
+                           <li class="card ${types[0]}">
+                           <img class="card-image" alt="${pokemon.name}"
+                           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
+                           <h2 class="card-tittle">${pokemon.id}. ${pokemon.name}</h2>
+                           <p class="card-subtittle"> ${types.join(" | ")} </p>
+                           </li>`;
+            return accumulator;
+        },
+        "");
+
+        const ul = document.querySelector('[data="pokedex"]');
+        ul.innerHTML = listPokemons;
+    })
 };
 
 fetchPokemon();
